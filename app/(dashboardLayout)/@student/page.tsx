46e8ea47fb3114938f9f590158d0f73ae7232080
IbraHim <mdibrahim752006@gmail.com>
2026-02-22 03:01:42 +0600

    fix parralal route error

---
+++ app/(dashboardLayout)/@student/page.tsx
@@ -0,0 +1,11 @@
+
+
+const StudentPage = () => {
+    return (
+        <div>
+            this is studnet page
+        </div>
+    );
+};
+
+export default StudentPage;
---
+++ app/(dashboardLayout)/@teacher/page.tsx
@@ -0,0 +1,11 @@
+
+
+const TeacherPage = () => {
+    return (
+        <div>
+            this is teacher page
+        </div>
+    );
+};
+
+export default TeacherPage;
